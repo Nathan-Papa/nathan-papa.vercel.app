35c5dbd0d2d30880af3a19927a666d8c5cc405c0
Nathan Papa <nathanpapa35@gmail.com>
2023-08-04 22:14:02 -0500

    Fixing title size

--- src/components/ClickerHeader.js
+++ src/components/ClickerHeader.js
@@ -4,7 +4,7 @@ import React from "react";
 const ClickerHeader = (props) => {
     return (
         <div className="min-h-[15%] lg:min-h-[20%] xl:min-h-[25%] flex flex-col justify-around">
-            <h1 className="text-3xl xl:text-5xl text-center text-[#FFFDE7]">Cannoli Clicker</h1>
+            <h1 className="text-3xl xl:text-4xl text-center text-[#FFFDE7]">Cannoli Clicker</h1>
             <div className="h-[45%] flex flex-col justify-between">
                 <p className="text-[#FFFDE7] text-center text-md xl:text-xl">{props.cannoli} cannoli</p>
                 <p className="text-[#FFFDE7] text-center text-md xl:text-xl">{Math.round(props.cps * props.cpsMultiplier)} cannoli per second</p>
